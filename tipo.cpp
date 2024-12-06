5a863287c2510770f3fb29fe05b3aeead47139d2
Cauã Pereira de Souza <caua.souza@ufv.br>
2024-12-06 09:19:29 -0300

    Update tipo.cpp

--- tipo.cpp
+++ tipo.cpp
@@ -1,7 +1,7 @@
 #include "tipo.h"
 
-Tipo::Tipo(std::string nome, int numRep, int relacoes[]): _nome(nome), _numRep(numRep) {
-    for(int i=0; i<18; i++)
+Tipo::Tipo(std::string nome, int numRep, float relacoes[]): _nome(nome), _numRep(numRep) {
+    for(int i = 0; i < 18; i++)
         _relacoes[i] = relacoes[i]; // Colocando todas as relações no tipo
 }
 
@@ -9,6 +9,6 @@ int Tipo::get_numRep(){
     return _numRep; 
 }
 
-int Tipo::get_relacoes(int tipo){
+float Tipo::get_relacoes(int tipo){
     return _relacoes[tipo]; // Retornando a relação com o tipo desejado
 }
