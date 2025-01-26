4403cad0783713f0d50d8c0595d70ab6f668f84a
Cauã Pereira de Souza <caua.souza@ufv.br>
2025-01-26 17:36:41 -0300

    Update Pokemon.hpp

--- Pokemon.hpp
+++ Pokemon.hpp
@@ -9,6 +9,10 @@
 #include "Tipo.hpp"
 #include "Golpe.hpp"
 
+// Forward declarations
+class Item;
+class Habilidade;
+
 class Pokemon {
     private:
         int _id; // Identifica o Pokémon
@@ -18,30 +22,36 @@ class Pokemon {
         Tipo _tipo1, _tipo2; // Tipos do Pokémon
         int _condicao; // 0 = nenhum, 1 = queimadura, 2 = congelamento, 3 = paralisia, 4 = veneno ou 5 = sono
         Golpe _golpe1, _golpe2, _golpe3, _golpe4; // Golpes atuais do Pokémon
+        Item *_item;
+        Habilidade *_habilidade;
 
-    public:
-        Pokemon(int id, std::string nome, int vida, int ataque, int defesa, int ataqueEspecial, int defesaEspecial, int velocidade, const Tipo &tipo1, const Tipo &tipo2, const Golpe &golpe1, const Golpe &golpe2, const Golpe &golpe3, const Golpe &golpe4);
-        int get_id();
-        std::string get_nome();
-        int get_estatistica(int estatistica); // Retorna uma das 6 estatísticas do Pokémon
-        int get_estagio(int estatistica); // Retorna o estágio de uma das 6 estatísticas do Pokémon
-        Tipo get_tipo1();
-        Tipo get_tipo2();
-        int get_tipo1Num();
-        int get_tipo2Num();
-        int get_relacaoTipo(Tipo tipo, int posicao); // Retorna a relação entre dois tipos
-        int get_condicao();
+        int _estatisticas_calculos[6];
 
-        void set_nome(std::string nome); // Para apelidar o Pokémon
-        void set_estatistica(int estatistica, int valor);  // Setar novo valor nas estatísticas
-        void set_condicao(int condicao); // Altera a condição do Pokémon
-        void set_estagio(int estatistica, int estagio); // Altera o estágio de uma das estatísticas do Pokémon
+public:
+    Pokemon(int id, std::string nome, int vida, int ataque, int defesa, int ataqueEspecial, int defesaEspecial, int velocidade, const Tipo &tipo1, const Tipo &tipo2, const Golpe &golpe1, const Golpe &golpe2, const Golpe &golpe3, const Golpe &golpe4);
 
-        float calculaDano(Golpe &golpe, Pokemon &pokemon); // Calcula o dano de um golpe contra um Pokémon
-        Golpe get_golpe1() const;
-        Golpe get_golpe2() const;
-        Golpe get_golpe3() const;
-        Golpe get_golpe4() const;
+    int get_id() const;
+    std::string get_nome() const;
+    int get_estatistica(int estatistica) const; // Retorna uma das 6 estatísticas do Pokémon
+    int get_estatistica_calculos(int estatistica) const;
+    int get_estagio(int estatistica) const; // Retorna o estágio de uma das 6 estatísticas do Pokémon
+    Tipo get_tipo1() const;
+    Tipo get_tipo2() const;
+    int get_tipo1Num();
+    int get_tipo2Num();
+    int get_relacaoTipo(Tipo tipo, int posicao) const; // Retorna a relação entre dois tipos
+    int get_condicao() const;
+    Golpe get_golpe1() const;
+    Golpe get_golpe2() const;
+    Golpe get_golpe3() const;
+    Golpe get_golpe4() const;
+
+    void set_nome(std::string nome); // Para apelidar o Pokémon
+    void set_estatistica(int estatistica, int valor);  // Setar novo valor nas estatísticas
+    void set_condicao(int condicao); // Altera a condição do Pokémon
+    void set_estagio(int estatistica, int estagio); // Altera o estágio de uma das estatísticas do Pokémon
+    
+    float calculaDano(Golpe &golpe, Pokemon &pokemon);
 };
 
 #endif
