71b46d5eb8ca216630d5c68b16ef90bf2e1783b2
Cauã Pereira de Souza <caua.souza@ufv.br>
2025-01-26 17:34:32 -0300

    Update Item.cpp

--- Item.cpp
+++ Item.cpp
@@ -1,7 +1,12 @@
 #include "Item.hpp"
+#include "Pokemon.hpp"
 
 Item::Item(std::string nome, std::string descricao) : _nome_item(nome), _descricao(descricao) {}
 
 std::string Item::get_nome_item() const {
-    return _nome_item;  
+    return _nome_item;
+}
+
+std::string Item::get_descricao_item() const {
+    return _descricao;
 }
