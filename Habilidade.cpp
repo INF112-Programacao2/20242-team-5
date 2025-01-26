0fb9b51f142d61ea2bbb4dd516823fa8aef106bf
Cauã Pereira de Souza <caua.souza@ufv.br>
2025-01-26 17:33:50 -0300

    Update Habilidade.cpp

--- Habilidade.cpp
+++ Habilidade.cpp
@@ -1,12 +1,6 @@
 #include "Habilidade.hpp"
-#include <cstdlib> // Para a função rand()
 
 Habilidade::Habilidade(std::string nome, std::string descricao) : _nome(nome), _descricao(descricao) {}
-
-Habilidade::Habilidade(std::string nome, std::string descricao) {
-    _nome = nome;
-    _descricao = descricao;
-}
 
 std::string Habilidade::get_nome() const {
     return _nome;
