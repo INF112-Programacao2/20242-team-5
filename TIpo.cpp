6a60b9c735e2996bd60bcf6aa782f2accf582c8e
Cauã Pereira de Souza <caua.souza@ufv.br>
2025-01-26 17:37:06 -0300

    Update TIpo.cpp

--- TIpo.cpp
+++ TIpo.cpp
@@ -6,20 +6,20 @@ std::string Tipo::get_nome(){
     return _nome;
 }
 
-void Tipo::set_nome(std::string nome){
-    _nome = nome;
-}
-
 int Tipo::get_numRep(){
     return _numRep;
 }
 
-void Tipo::set_numRep(int numRep){
-    _numRep = numRep;
-}
-
 float Tipo::get_relacao(int tipo1, int tipo2){
     return _relacoes[tipo1][tipo2]; // Retornando a relação com o tipo desejado
+}
+
+void Tipo::set_nome(std::string nome){
+    _nome = nome;
+}
+
+void Tipo::set_numRep(int numRep){
+    _numRep = numRep;
 }
 
 void Tipo::set_relacao(float valor, int tipo1, int tipo2){
