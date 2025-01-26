89fd45fb827accb973d2ace19628c1c40554576f
Cauã Pereira de Souza <caua.souza@ufv.br>
2025-01-26 17:35:48 -0300

    Update ModificaEst.hpp

--- ModificaEst.hpp
+++ ModificaEst.hpp
@@ -8,14 +8,18 @@ class ModificaEst : public Golpe{ // Uma espécie de golpes que modifica as esta
         int _estatisticasAfetadas[6]; // Vetor com os estágios para cada estatística que será afetada. Estágios vão de -6 até 6, -6 representando 2/8 (2/8 = 2/(2+6) = 25%) do original, 0 representando 2/2 (100%) do original e 6 representando 8/2 (8/2 = (2+6)/2 = 400%) do original. 0 = PV (não utilizado), 1 = ataque, 2 = defesa, 3 = ataque especial, 4 = defesa especial e 5 = velocidade.
         int _chance; // Chance de modificar as estatísticas
         bool _autoAlvo; // Determina se afeta o oponente ou o próprio usuário
+
     public:
         ModificaEst(); // Construtor
-        int* get_estatisticasAfetadas();
+
+        int *get_estatisticasAfetadas();
+        int get_chance() const;
+
         void set_estatisticasAfetadas(int estatistica, int estagio);
-        int get_chance();
         void set_chance(int chance);
-        bool is_autoAlvo();
         void set_autoAlvo(int autoAlvo);
+
+        bool is_autoAlvo();
 };
 
 #endif
